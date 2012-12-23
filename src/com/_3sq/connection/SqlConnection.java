25e4b900440c496ec33bd6add551640bf9a2b218
vishy2907 <bansode.vishal1@gmail.com>
2012-12-23 09:10:05 -0800

    SqlConnection.java Added By Vishal

--- src/com/_3sq/connection/SqlConnection.java
+++ src/com/_3sq/connection/SqlConnection.java
@@ -1,28 +1,52 @@
 package com._3sq.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
+//import com.mysql.jdbc.Connection;
+
 /**
  * 
- * @author Vishal B
- * Handles All MySQL Connections - Singleton Class
+ * @author Vishal B Handles All MySQL Connections - Singleton Class
  */
 public class SqlConnection {
-	
-	private static SqlConnection 				m_cSqlConnection;
-	
-	
-	
+
+	private static Connection m_cSqlConnection;
+
 	private SqlConnection()
 	{
 		
 	}
-	
-	public static SqlConnection getSqlConnection()
-	{
-		if(m_cSqlConnection == null)
-			return new SqlConnection();
+	private Connection CreateNewConnection() {
+		try {
+			String driverName = "com.mysql.jdbc.Driver";
+			Class.forName(driverName);
+
+			String serverName = "localhost:3306";
+			String mydatabase = "gym db";
+
+			String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+			String username = "root";
+			String password = "gracious1!";
+
+			m_cSqlConnection = DriverManager.getConnection( url, username, password );
+						
+		} catch (Exception e) {
+				System.err.println("SqlConnection.java : ");
+			e.printStackTrace();
+		}
+		return m_cSqlConnection;
+	}
+
+	public static Connection getSqlConnection() {
+		if (m_cSqlConnection == null)
+			return new SqlConnection().CreateNewConnection();
 		else
 			return m_cSqlConnection;
 	}
-	
-	
 }
